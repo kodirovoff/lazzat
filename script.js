1904813bf39beb892407f90f7e7cd4bc31b0ce90
Adkham <83287524+kodirovoff@users.noreply.github.com>
2021-12-05 05:07:30 +0900

    Add app

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// document.querySelector('.number-show').addEventListener('click', function() {
+//     document.querySelector('.number').textContent = +998934955855;
+// });
+// document.getElementById("nav-links").addEventListener('click', function(){
+//     show
+// })
+
+if(document.getElementById('notifications').style.width = '100%'){
+}
